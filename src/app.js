528176153b1458de4fab5f7d4eb293f89262afce
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-11-11 10:42:43 +0700

    complete  without mvc

--- src/app.js
+++ src/app.js
@@ -1,15 +1,46 @@
 const express = require("express"),
   fs = require("fs"),
-  exphbs = require("express-handlebars"),
-  app = express(),
-  port = 3000;
-app.use(express.static(__dirname + "/public"));
+  hbs = require("express-handlebars"),
+  path = require("path");
+bodyparser = require("body-parser");
+(app = express()), (port = 3000);
 
-app.engine("hbs", exphbs.engine());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: ".hbs",
+    defaultLayout: "container.hbs",
+  })
+);
 app.set("view engine", "hbs");
-
+// app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "resources//views"));
 app.get("/", (req, res) => {
-  res.send("Hello Word");
+  res.render("home");
+});
+app.post("/calculationMVCpattern", function (req, res) {
+  let rs = 0;
+  const x = parseFloat(req.body.valueX);
+  const y = parseFloat(req.body.valueY);
+  switch (req.body.operator) {
+    case "sum":
+      rs = x + y;
+      break;
+    case "subtract":
+      rs = x - y;
+      break;
+    case "multi":
+      rs = x * y;
+      break;
+    case "divide":
+      rs = x / y;
+      break;
+  }
+  res.render("home", {
+    result: rs,
+  });
 });
 
 app.listen(port, () => {
